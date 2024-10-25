a101af5e3059910fa2aeee9f9b3a175a5013f63d
BogdanPiligrim <151871854+BogdanPiligrim@users.noreply.github.com>
2024-10-25 18:19:07 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("playLink").addEventListener("click", function(event) {
+    event.preventDefault(); // Предотвращаем переход по ссылке
+    alert("Гра скоро почнеться! Підготуйтеся до солодких виграшів у Sweet Bonanza 1000!"); // Всплывающее сообщение
+  });
